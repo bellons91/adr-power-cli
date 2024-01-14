d035a645baf0b77a0140fb5163580a7ca8f39cdc
Davide Bellone <d.bellone@avanade.com>
2024-01-14 16:32:51 +0100

    Renamed project

---
+++ cli/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using Core;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+IHost host = CreateHost();
+ISender sender = host.Services.GetRequiredService<ISender>();
+
+var result = await sender.Send(new TestOperation.TestRequest { Name = "Davide" });
+Console.WriteLine(result);
+
+static IHost CreateHost() =>
+Host
+.CreateDefaultBuilder()
+.ConfigureServices((context, services) =>
+{
+    services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<TestOperation>());
+})
+
+.Build();
---
+++ core/TestHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace Core
+{
+    public class TestOperation
+    {
+        public class TestRequest : IRequest<string>
+        {
+            public string Name { get; set; }
+        }
+
+        public class TestHandler : IRequestHandler<TestRequest, string>
+        {
+            public Task<string> Handle(TestRequest request, CancellationToken cancellationToken)
+            {
+                return Task.FromResult($"test {request.Name}");
+            }
+        }
+    }
+}
