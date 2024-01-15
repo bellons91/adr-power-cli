ebff73ec941f41ee9fe77c0fbc413d332339a126
Davide Bellone <d.bellone@avanade.com>
2024-01-15 11:47:53 +0100

    Added unit tests

---
+++ tests/adr-core-tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace adr_core_tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
